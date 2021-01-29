0965610339eb74c60e2ddabd57142b2aa6605964
runz0rEgor <egor31340@gmail.com>
2021-01-29 12:31:23 +0300

    init app

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Header: React.FC = () => {
+  return (
+    <header>
+      <h1>Hellow header</h1>
+    </header>
+  )
+}
---
+++ src/components/main/Main.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Main: React.FC = () => {
+  return (
+    <main>
+      <h1>hello from main</h1>
+    </main>
+  )
+}
